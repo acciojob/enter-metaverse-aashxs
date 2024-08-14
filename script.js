eb395dec4cc1286051c157376d25bd785cd899fb
Ashish <90548985+aashxs@users.noreply.github.com>
2024-08-14 16:03:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let btn=document.querySelector("#enterbtn");
+let inc=document.querySelector("h1");
+
+btn.addEventListener("click",()=>{
+    
+    inc.innerText="Entered Metaverse";
+   
+    
+    
+
+})
